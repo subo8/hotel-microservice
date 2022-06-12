16cf45e75f0dd3a3aae0e73f37cf383cedc42e80
Khosbayar <xocoooo@gmail.com>
2022-06-12 18:14:17 -0500

    add apigateway

--- BackEnd/api-gateway/src/main/java/com/sa/api/gateway/ApiGatewayApplication.java
+++ BackEnd/api-gateway/src/main/java/com/sa/api/gateway/ApiGatewayApplication.java
@@ -4,12 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@SpringBootApplication
 @EnableZuulProxy
+@SpringBootApplication
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
+
 	}
 
 }
